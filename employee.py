d49d830b1bfb82d7cc0b42cc549d2a6cdb4eed90
oblisstified <k19079055@kcl.ac.uk>
2022-10-11 20:07:31 +0100

    finish hopefully

--- employee.py
+++ employee.py
@@ -17,7 +17,7 @@ class Employee:
         initialPay =0
         if self.contract == "hourly":
             initialPay = self.hours * self.pay
-        elif self.contract == "salary":
+        elif self.contract == "monthly":
             initialPay = self.monthlySal
         if self.commission == None:
             return initialPay
@@ -30,13 +30,12 @@ class Employee:
 
     def __str__(self):
         string =   f"{self.name} works on a "
-        print (string)
         if self.contract == "hourly":
             string += f"contract of {self.hours} hours at {self.pay}/hour"
         else:
             string += f"monthly salary of {self.monthlySal}"
         if self.commission == "contract":
-            string += f" and receives a  commission for {self.contractNum} contract(s) at {self.contractPay}/contract."
+            string += f" and receives a commission for {self.contractNum} contract(s) at {self.contractPay}/contract."
         elif self.commission == "bonus":
             string += f" and receives a bonus commission of {self.bonusCommission}."
         else:
@@ -55,14 +54,14 @@ class Employee:
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Employee('Billie',"salary", None, None, None,4000,None,None,None)
-print(billie.get_pay(),billie.__str__())
+
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Employee('Charlie',"hourly" , None, 100 , 25,None, None,None, None)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = Employee('Renee', "monthly", "contract", None, None,3000,4,200,None)
-
+print(renee.get_pay(),renee.__str__())
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = Employee('Jan',"hourly", "contract", 150,25,None,3,220,None)
 
